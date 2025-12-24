492f0b2d2b7232c085a1bbe93057cdad99f360a6
hrqmartins <hrqmartins129@gmail.com>
2025-12-24 18:52:24 -0300

    feat: Criando regra para horário suspeito.

---
+++ src/br/com/sentinelbank/regras/RegraHorarioSuspeito.java
@@ -0,0 +1,26 @@
+package br.com.sentinelbank.regras;
+
+import br.com.sentinelbank.beans.Transacao;
+import br.com.sentinelbank.interfaces.RegraSeguranca;
+
+public class RegraHorarioSuspeito implements RegraSeguranca {
+
+    @Override
+    public boolean verificarTransacao(Transacao transacao) {
+        if (transacao.getHorario().getHour() < 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int pesoRisco() {
+        return 40;
+    }
+
+
+
+
+
+}
